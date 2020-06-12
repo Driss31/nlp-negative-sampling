34b2a8d31de0765c3f98a7ebf0fc0c2552cb09be
Driss Debbagh-Nour <driss.debbaghnour@free-now.com>
2020-06-12 11:25:46 +0200

    Add tests for parser.

--- tests/utils/test_parser.py
+++ tests/utils/test_parser.py
@@ -1,1 +1,20 @@
 """Tests for parser."""
+import unittest
+
+from nlp_negative_sampling.utils.parser import get_command_line_parser
+
+
+class ParserTest(unittest.TestCase):
+    """Class to test parser."""
+
+    def setUp(self):
+        """Set up a parser."""
+        self.parser = get_command_line_parser()
+
+    def test_get_command_line_parser(self):
+        """Should return a parser with given arguments."""
+        parsed = self.parser.parse_args(
+            ["--text_path", "text.txt", "--model_path", "train"]
+        )
+        self.assertEqual(parsed.text_path, "text.txt")
+        self.assertEqual(parsed.model_path, "train")
